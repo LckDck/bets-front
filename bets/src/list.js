51710cc2d52f7426ecce1bdec450a20d3b88c621
LckDck <5580093+LckDck@users.noreply.github.com>
2023-02-26 19:43:46 +0200

    2023 list

--- bets/src/list.js
+++ bets/src/list.js
@@ -12,92 +12,92 @@ export const list =
         },
         {
             id: 2,
-            label: 'Lewis Hamilton',
-            value: 'Hamilton'
-        },
-        {
-            id: 3,
-            label: 'George Russell',
-            value: 'Russell'
-        },
-        {
-            id: 4,
             label: 'Charles Leclerc',
             value: 'Leclerc'
         },
         {
-            id: 5,
+            id: 3,
             label: 'Carlos Sainz',
             value: 'Sainz'
         },
         {
+            id: 4,
+            label: 'Lewis Hamilton',
+            value: 'Hamilton'
+        },            
+        {
+            id: 5,
+            label: 'George Russell',
+            value: 'Russell'
+        },
+        {
             id: 6,
-            label: 'Daniel Ricciardo',
-            value: 'Ricciardo'
-        },
-        {
-            id: 7,
-            label: 'Lando Norris',
-            value: 'Norris'
-        },
-        {
-            id: 8,
-            label: 'Fernando Alonso',
-            value: 'Alonso'
-        },
-        {
-            id: 9,
             label: 'Esteban Ocon',
             value: 'Ocon'
-        },
+        }, 
         {
-            id: 10,
+            id: 7,
             label: 'Pierre Gasly',
             value: 'Gasly'
         },
         {
-            id: 11,
-            label: 'Yuki Tsunoda',
-            value: 'Tsunoda'
-        },
+            id: 8,
+            label: 'Lando Norris',
+            value: 'Norris'
+        },                          
         {
-            id: 12,
-            label: 'Sebastian Vettel',
-            value: 'Vettel'
-        },
+            id: 9,
+            label: 'Oscar Piastri',
+            value: 'Piastri'
+        }, 
         {
-            id: 13,
-            label: 'Lance Stroll',
-            value: 'Stroll'
-        },
-        {
-            id: 14,
-            label: 'Alex Albon',
-            value: 'Albon'
-        },
-        {
-            id: 15,
-            label: 'Nicholas Latifi',
-            value: 'Latifi'
-        },
-        {
-            id: 16,
+            id: 10,
             label: 'Valtteri Bottas',
             value: 'Bottas'
         },
         {
-            id: 17,
+            id: 11,
             label: 'Guanyu Zhou',
             value: 'Zhou'
         },
         {
+            id: 12,
+            label: 'Lance Stroll',
+            value: 'Stroll'
+        },
+        {
+            id: 13,
+            label: 'Fernando Alonso',
+            value: 'Alonso'
+        }, 
+        {
+            id: 14,
+            label: 'Kevin Magnussen',
+            value: 'Magnussen'
+        }, 
+        {
+            id: 15,
+            label: 'Nico Hülkenberg',
+            value: 'Hülkenberg'
+        },
+        {
+            id: 16,
+            label: 'Nyck de Vries',
+            value: 'de Vries'
+        },
+        {
+            id: 17,
+            label: 'Yuki Tsunoda',
+            value: 'Tsunoda'
+        },
+        {
             id: 18,
-            label: 'Mick Schumacher',
-            value: 'Schumacher'
+            label: 'Alex Albon',
+            value: 'Albon'
         },
         {
             id: 19,
-            label: 'Kevin Magnussen',
-            value: 'Magnussen'
+            label: 'Logan Sargeant',
+            value: 'Sargeant'
         }
     ];
