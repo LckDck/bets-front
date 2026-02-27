b17b2eae26a2bd3248eb506dc761de58ee050832
Oxana Grishchenko <grishchenko@burodata.com>
2026-02-27 22:45:54 +0500

    2026

--- bets/src/list.js
+++ bets/src/list.js
@@ -1,103 +1,117 @@
+
+
+
+
 export const list =
     [
         {
             id: 0,
-            label: 'Max Verstappen',
-            value: 'Verstappen'
+            label: 'Lando Norris',
+            value: 'Norris'
         },
         {
             id: 1,
-            label: 'Sergio Perez',
-            value: 'Perez'
-        },
-        {
-            id: 2,
-            label: 'Lewis Hamilton',
-            value: 'Hamilton'
-        },
-        {
-            id: 3,
-            label: 'George Russell',
-            value: 'Russell'
-        },
-        {
-            id: 4,
-            label: 'Charles Leclerc',
-            value: 'Leclerc'
-        },            
-        {
-            id: 5,
-            label: 'Carlos Sainz',
-            value: 'Sainz'
-        },
-        {
-            id: 6,
-            label: 'Lando Norris',
-            value: 'Norris'
-        }, 
-        {
-            id: 7,
             label: 'Oscar Piastri',
             value: 'Piastri'
         },
         {
+            id: 2,
+            label: 'George Russell',
+            value: 'Russell'
+        },
+        {
+            id: 3,
+            label: 'Kimi Antonelli',
+            value: 'Antonelli'
+        },
+        {
+            id: 4,
+            label: 'Max Verstappen',
+            value: 'Verstappen'
+        },            
+        {
+            id: 5,
+            label: 'Isack Hadjar',
+            value: 'Hadjar'
+        },
+        {
+            id: 6,
+            label: 'Charles Leclerc',
+            value: 'Leclerc'
+        }, 
+        {
+            id: 7,
+            label: 'Lewis Hamilton',
+            value: 'Lewis Hamilton'
+        },
+        {
             id: 8,
-            label: 'Fernando Alonso',
-            value: 'Alonso'
+            label: 'Alexander Albon',
+            value: 'Albon'
         },                          
         {
             id: 9,
+            label: 'Carlos Sainz',
+            value: 'Sainz'
+        }, 
+        {
+            id: 10,
+            label: 'Liam Lawson',
+            value: 'Lawson'
+        },
+        {
+            id: 11,
+            label: 'Arvid Lindblad',
+            value: 'Lindblad'
+        },
+        {
+            id: 12,
+            label: 'Fernando Alonso',
+            value: 'Alonso'
+        },
+        {
+            id: 13,
             label: 'Lance Stroll',
             value: 'Stroll'
         }, 
         {
-            id: 10,
+            id: 14,
             label: 'Esteban Ocon',
             value: 'Ocon'
+        }, 
+        {
+            id: 15,
+            label: 'Oliver Bearman',
+            value: 'Bearman'
         },
         {
-            id: 11,
+            id: 16,
+            label: 'Nico Hulkenberg',
+            value: 'Hulkenberg'
+        },
+        {
+            id: 17,
+            label: 'Gabriel Bortoleto',
+            value: 'Bortoleto'
+        },
+        {
+            id: 18,
             label: 'Pierre Gasly',
             value: 'Gasly'
         },
         {
-            id: 12,
-            label: 'Alexander Albon',
-            value: 'Albon'
+            id: 19,
+            label: 'Franco Colapinto',
+            value: 'Colapinto'
         },
         {
-            id: 13,
-            label: 'Logan Sargeant',
-            value: 'Sargeant'
-        }, 
-        {
-            id: 14,
-            label: 'Yuki Tsunoda',
-            value: 'Tsunoda'
-        }, 
-        {
-            id: 15,
-            label: 'Daniel Ricciardo',
-            value: 'Ricciardo'
+            id: 20,
+            label: 'Sergio Perez',
+            value: 'Perez'
         },
         {
-            id: 16,
+            id: 21,
             label: 'Valtteri Bottas',
             value: 'Bottas'
-        },
-        {
-            id: 17,
-            label: 'Guanyu Zhou',
-            value: 'Zhou'
-        },
-        {
-            id: 18,
-            label: 'Kevin Magnussen',
-            value: 'Magnussen'
-        },
-        {
-            id: 19,
-            label: 'Nico Hulkenberg',
-            value: 'Hulkenberg'
         }
     ];
